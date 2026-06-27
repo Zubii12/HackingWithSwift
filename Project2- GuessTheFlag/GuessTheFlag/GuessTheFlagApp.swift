82e29c5bc5e7208142cf3f652d6458aa83b1e15f
Zubii <ionutzubascu@yahoo.com>
2026-06-27 22:45:23 +0300

    add GuessTheFlag project

---
+++ Project2- GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Ionut Zubascu on 27.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

112f9b5b7a897266be9cfa6f9cee5ca175729c27
Zubii <ionutzubascu@yahoo.com>
2026-07-01 00:19:24 +0300

    add WordScarmble project

---
+++ Project5 - WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Ionut Zubascu on 30.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

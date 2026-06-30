0ddbff445bb584809e250ab0ab48cb05107515a4
Zubii <ionutzubascu@yahoo.com>
2026-06-30 18:54:24 +0300

    add BetterRest project

---
+++ Project4 - BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Ionut Zubascu on 30.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

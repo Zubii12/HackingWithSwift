e842ea1d2428d8896abbb4d14e06d1c2f711cf40
Zubii <ionutzubascu@yahoo.com>
2026-07-01 16:01:45 +0300

    add Moonshot project

---
+++ Project8 - Moonshot/Moonshot/Astrounaut.swift
@@ -0,0 +1,12 @@
+//
+//  Astrounaut.swift
+//  Moonshot
+//
+//  Created by Ionut Zubascu on 01.07.2026.
+//
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ Project8 - Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Ionut Zubascu on 01.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
